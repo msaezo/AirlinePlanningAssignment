9be9c511c268421dca98df05be5843b49ec63ee9
Miguel Angel <miansaor@gmail.com>
2019-11-28 12:48:58 +0100

    Demand updated, still needs revision

--- DemandForecast.m
+++ DemandForecast.m
@@ -1,21 +1,46 @@
 %Demand forecast
+clc
+clear all
 
-x0 = [];
-lb = [];
-ub = [];
-f=@(k,b1,b2,b3) LSE_demand(k,b1,b2,b3);
+%Initial Read
+ pop_2014 = xlsread('AE4423_Datasheets.xls','General','B4:B23');
+    GDP_2014 = xlsread('AE4423_Datasheets.xls','General','F4:F23');
+    Demand_2014 = xlsread('AE4423_Datasheets.xls','Group 31','C13:V32');
+    Lat = xlsread('AE4423_Datasheets.xls','Group 31','C6:V6');
+    Lon = xlsread('AE4423_Datasheets.xls','Group 31','C7:V7');
+    
+    f_cost = 1.42;
+    N = length(Lat);
+    RE = 6371; % Radius of the Earth [km]
+    Distance = zeros(N);
+    Dsigma = zeros(N);
+    
+    for i = 1:N
+        for j = 1:N
+            Dsigma(i,j) = 2*asin(sqrt((sind((Lat(i)-Lat(j))/2))^2+cosd(Lat(i))*...
+                cosd(Lat(j))*(sind((Lon(i)-Lon(j))/2))^2));
+            Distance(i,j) = RE*Dsigma(i,j); % [km]
+        end
+    end
+
+    
+% Fmincon
+x0 = [1,1,1,1];
+lb = [0,0,0,0];
+ub = [1,1,1,1];
+f=@(k,b1,b2,b3) LSE_demand(k,b1,b2,b3,pop_2014,GDP_2014,Demand_2014,Distance,N,f_cost,0);
 
 options=optimset('Display','iter','Algorithm','sqp');
-fun=@(x) f(x(1),x(2),x(3),x(4)); % [deg,deg,m,deg,m,m]
+fun=@(x) f(x(1),x(2),x(3),x(4)); % [k,b1,b2,b3]
 
 % Options for the optimization
 options.Display         = 'iter-detailed';
 options.Algorithm       = 'sqp';
 options.FunValCheck     = 'off';
-options.DiffMinChange   = 1e-4;         % Minimum change while gradient searching
-options.DiffMaxChange   = 1e-3;         % Maximum change while gradient searching
+options.DiffMinChange   = 1e-5;         % Minimum change while gradient searching
+options.DiffMaxChange   = 1e-4;         % Maximum change while gradient searching
 options.TolCon          = 1e-6;         % Maximum difference between two subsequent constraint vectors [c and ceq]
-options.TolFun          = 1e-5;         % Maximum difference between two subsequent objective value
+options.TolFun          = 1e-6;         % Maximum difference between two subsequent objective value
 options.TolX            = 1e-6;         % Maximum difference between two subsequent design vectors
 options.PlotFcns        = {@optimplotx,@optimplotfunccount,@optimplotfval,@optimplotconstrviolation,@optimplotstepsize,@optimplotfirstorderopt};
 options.MaxIter         = 500;           % Maximum iterations
@@ -23,3 +48,5 @@ options.MaxIter         = 500;           % Maximum iterations
 tic;
 [x,fval,exitflag,output]=fmincon(fun,x0,[],[],[],[],lb,ub,[],options);
 toc;
+
+Demand_est = LSE_demand(x(1),x(2),x(3),x(4),pop_2014,GDP_2014,Demand_2014,Distance,N,f_cost,1);
--- LSE_demand.m
+++ LSE_demand.m
@@ -1,5 +1,37 @@
-function[LSE] = LSE_demand(k,b1,b2,b3)
-    
+function[LSE] = LSE_demand(k,b1,b2,b3,pop_2014,GDP_2014,Demand_2014,Distance,N,f_cost,Mode)
+    if Mode == 0
+        %LSE = 0;
+        for i = 1:N
+            for j = 1:N
+                if i == j 
+                    Demand_est(i,j) = 0;
+                else
+                    Demand_est(i,j) = k*((pop_2014(i)*pop_2014(j))^b1*(GDP_2014(i)*GDP_2014(j))^b2)...
+                        /(f_cost*Distance(i,j))^b3;
+                end
+                %LSE = LSE + sqrt(abs(Demand_2014(i,j)^2-Demand_est(i,j)^2));
+            end
+        end
+       LSE_mat = sqrt(abs(Demand_est.^2 - Demand_2014^2));
+       LSE = sum( LSE_mat , 'all' ); 
+       LSE = LSE/N;
 
-
+    else
+        LSE = 0;
+        for i = 1:N
+            for j = 1:N
+                if i == j 
+                    Demand_est(i,j) = 0;
+                else
+                    Demand_est(i,j) = k*((pop_2014(i)*pop_2014(j))^b1*(GDP_2014(i)*GDP_2014(j))^b2)...
+                        /(f_cost*Distance(i,j))^b3;
+                end
+                %LSE = LSE + sqrt(abs(Demand_2014(i,j)^2-Demand_est(i,j)^2));
+            end
+        end
+       LSE_mat = abs(Demand_est.^2 - Demand_2014^2);
+       LSE = sum(LSE_mat,'all'); 
+       LSE = LSE/N;
+       LSE = Demand_est;
+    end
 end
