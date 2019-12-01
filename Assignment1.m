d30f57a9f70dd9ab482b077006ffa3223b1b6f11
Guillermo <guillermolarajuan@gmail.com>
2019-12-01 23:41:39 +0100

    Problem 1 + new excel

--- Assignment1.m
+++ Assignment1.m
@@ -1,22 +1,24 @@
 %% Assignment 1 - (AE4423-19 Airline Planning and Optimization)
 clc, clear all, close all
-addpath('C:\Program Files\IBM\ILOG\CPLEX_Studio_Community129\cplex\matlab\x64_win64')
+% addpath('C:\Program Files\IBM\ILOG\CPLEX_Studio_Community129\cplex\matlab\x64_win64')
+addpath('C:\Program Files\IBM\ILOG\CPLEX_Studio129\cplex\matlab\x64_win64')
 
 %% Data
 
-f = 1.42; % [USD/gallon] 2019
+f = 1.42; % [USD/gallon] 2019 Assuming the same value for the 2014
 LF = 0.75; % Load Factor
+BT = 10*7; % [h/week]
 
 %% Data from excel
 
 % Population: It has to be multiplied by 1000, because those values are 
 % given per 1000 inhabitants
 pop_2014 = xlsread('AE4423_Datasheets.xls','General','B4:B23')*1000; % Population 2014
-pop_2019 = xlsread('AE4423_Datasheets.xls','General','C4:C23')*1000; % Population 2019
+pop_2017 = xlsread('AE4423_Datasheets.xls','General','C4:C23')*1000; % Population 2019
 
 % Gross Domestic Product
 GDP_2014 = xlsread('AE4423_Datasheets.xls','General','F4:F23'); % GDP 2014 [USD]
-GDP_2019 = xlsread('AE4423_Datasheets.xls','General','G4:G23'); % GDP 2019 [USD]
+GDP_2017 = xlsread('AE4423_Datasheets.xls','General','G4:G23'); % GDP 2019 [USD]
 
 % Airport data
 [~,Cities,~] = xlsread('AE4423_Datasheets.xls','Group 31','C4:V4'); % Name of cities
@@ -45,7 +47,7 @@ N = length(Airports); % Number of airports
 
 Speed = [550 820 850 870]; % [km/h]
 Seats = [45 70 150 320]; 
-Average_TAT = [25 35 45 60]/60; % Turn Around Time [mins --> h]
+TAT = [25 35 45 60]/60; % Turn Around Time [mins --> h]
 Max_range = [1500 3300 6300 12000]; % Maximum Range [km]
 Rwy_req = [1400 1600 1800 2600]; % Runway required [m]
 
@@ -58,6 +60,29 @@ CF = [1.0 2.0 3.75 9.0]; % Fuel cost parameter CF
 AC_number = length(Speed); % Number of aircraft
 
 %% Pre-processing
+
+% Population --> Linear regression y=m*Year+n
+m_pop = zeros(1,N);
+n_pop = zeros(1,N);
+
+for i = 1:N
+    m_pop(i) = (pop_2017(i)-pop_2014(i))/(2017-2014);
+    n_pop(i) = pop_2017(i)-m_pop(i)*2017;
+end
+
+pop_2019 = m_pop*2019+n_pop;
+
+% Gross Domestic Product --> Linear regression y=m*Year+n
+m_GDP = zeros(1,N);
+n_GDP = zeros(1,N);
+
+for i = 1:N
+    m_GDP(i) = (GDP_2017(i)-GDP_2014(i))/(2017-2014);
+    n_GDP(i) = GDP_2017(i)-m_GDP(i)*2017;
+end
+
+GDP_2019 = m_GDP*2019+n_GDP;
+
 
 % Distance
 RE = 6371; % Radius of the Earth [km]
@@ -177,9 +202,6 @@ for i = 1:N
         end
     end
 end
-% NaNvector = find(isnan(obj))--> Used to check if there were any cell with
-% NaN value. Found when 0^(-0.76) when computing the Yield. I think that
-% that problem is solved.
 
 lb = zeros(DV,1);
 ub = inf(DV,1);
@@ -187,21 +209,143 @@ obj = obj';
 ctype1 = char(ones(1, (DV)) * ('I'));  %I=integer. Other options, C=continous, B=binary.
 ctype = strcat(ctype1);
 
-cplex.addCols(obj, [], lb, ub, ctype,NameDV); %This can also be done without NameDV
+cplex.addCols(obj, [], lb, ub, ctype); %This can also be done without NameDV
 
 %% Constraint 1: Demand verification
-
-
-for i=1:N
-    for j=1:N
+C1_matrix = zeros(N^2,DV);
+row_c1 = 1;
+for i = 1:N
+    for j = 1:N
         C1 = zeros(1,DV);
         C1(varindex_3(1,i,j,k,N,AC_number)) = 1;
         C1(varindex_3(2,i,j,k,N,AC_number)) = 1;
-        RHS = Demand(i,j);
-        cplex.addRows(-inf, C1, Demand(i,j),sprintf('Constraint1%d_%d',i,j));
+        RHS = Demand_2019(i,j);
+        cplex.addRows(-inf,C1,RHS,sprintf('Constraint1%d_%d',i,j));
+        C1_matrix(row_c1,:) = C1; 
+        row_c1 = row_c1 + 1;
     end
 end
 
+%% Constraint 2: Making sure that w is 0 if the origin or the destination 
+% is the hub
+C2_matrix = zeros(N^2,DV);
+row_c2 = 1;
+for i = 1:N
+    for j = 1:N
+        C2 = zeros(1,DV);
+        C2(varindex_3(1,i,j,k,N,AC_number)) = 1;
+        RHS = Demand_2019(i,j)*g(i)*g(j);
+        cplex.addRows(-inf,C2, RHS,sprintf('Constraint2%d_%d',i,j));
+        C2_matrix(row_c2,:) = C2; 
+        row_c2 = row_c2 + 1;
+    end
+end
+
+%% Constraint 3: Capacity
+C3_matrix = zeros(N^2,DV);
+row_c3 = 1;
+for i = 1:N
+    for j = 1:N
+        C3 = zeros(1,DV);
+        C3(varindex_3(2,i,j,k,N,AC_number)) = 1;
+        
+        for m = 1:N
+            C3(varindex_3(1,i,m,k,N,AC_number)) = C3(varindex_3(1,i,m,k,N,AC_number))+(1-g(j));
+            C3(varindex_3(1,m,j,k,N,AC_number)) = C3(varindex_3(1,i,m,k,N,AC_number))+(1-g(i));
+        end
+        
+        
+        for k = 1:AC_number
+            C3(varindex_3(3,i,j,k,N,AC_number)) = -Seats(k)*LF;
+        end
+        C3_matrix(row_c3,:) = C3; 
+        RHS = 0;
+        cplex.addRows(-inf, C3, RHS,sprintf('Constraint3%d_%d',i,j));
+        row_c3 = row_c3 + 1;
+    end
+end
+
+%% Constraint 4
+C4_matrix = zeros(N*AC_number,DV);
+row_c4 = 1;
+for k = 1:AC_number
+    for i = 1:N
+        C4 = zeros(1,DV);
+        for j = 1:N
+            if i ~= j
+                C4(varindex_3(3,i,j,k,N,AC_number)) = 1;
+                C4(varindex_3(3,j,i,k,N,AC_number)) = -1;
+            end
+        end
+        C4_matrix(row_c4,:) = C4; 
+        cplex.addRows(0,C4,0,sprintf('Constraint4%d_%d',k,i));
+        row_c4 = row_c4 + 1;
+    end
+end
+
+%% Constraint 5
+
+C5_matrix = zeros(AC_number,DV);
+row_c5 = 1;
+for k = 1:AC_number
+    C5 = zeros(1,DV);
+    for i = 1:N
+        for j = 1:N % if i=j the distance is 0 so the first variable should be zero? 
+            C5(varindex_3(3,i,j,k,N,AC_number)) = Distance(i,j)/Speed(k) + TAT(k)*(1+(1-g(j)));
+            C5(varindex_3(4,i,j,k,N,AC_number)) = -BT;
+        end
+    end
+    C5_matrix(row_c5,:) = C5;
+    cplex.addRows(-inf,C5,0,sprintf('Constraint5%d',k));
+    row_c5 = row_c5 + 1;
+end
+
+%% Constraint 6
+C6_matrix = zeros(N^2*AC_number,DV);
+row_c6 = 1;
+for k = 1:AC_number 
+    for i = 1:N
+        for j = 1:N
+            C6 = zeros(1,DV);
+            C6(varindex_3(3,i,j,k,N,AC_number)) = 1;
+            RHS = 0;
+            if Distance(i,j) <= Max_range(k)
+                RHS = 1000000;                
+            end
+            cplex.addRows(-inf, C6, RHS,sprintf('Constraint6%d_%d_%d',k,i,j));
+            C6_matrix(row_c6,:) = C6;
+            row_c6 = row_c6 + 1;
+        end
+    end
+end
+
+%% Constraint 7
+C7_matrix = zeros(N^2*AC_number,DV);
+row_c7 = 1;
+for k = 1:AC_number 
+    for i = 1:N
+        for j = 1:N
+            C7 = zeros(1,DV);
+            C7(varindex_3(3,i,j,k,N,AC_number)) = 1;
+            RHS = 0;
+            if Runway_length(i) >= Rwy_req(k) && Runway_length(j) >= Rwy_req(k)
+                RHS = 1000000;                
+            end
+            cplex.addRows(-inf, C7, RHS,sprintf('Constraint7%d_%d_%d',k,i,j));
+            C7_matrix(row_c7,:) = C7;
+            row_c7 = row_c7 + 1;
+        end
+    end
+end
+
+% %% Constraint 8
+% C8_matrix = zeros(1,DV);
+% row_c8 = 1;
+% C8 = zeros(1,DV);
+% for k = 1:AC_number 
+%     C8(varindex_3(4,i,j,k,N,AC_number)) = 1;
+% end
+% cplex.addRows(-inf, C8, inf,sprintf('Constraint8'));
 
 %% Solve the model
 
@@ -217,4 +361,6 @@ cplex.solve();
 %         end
 %     end
 % end
-            
+obj_matrix=cplex.Model.obj;
+A_total=cplex.Model.A;
+RHS_total=cplex.Model.rhs;
