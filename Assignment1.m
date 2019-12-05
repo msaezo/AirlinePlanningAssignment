c77eb1f046e62450d50008767a659294e12dcabb
Miguel Angel <miansaor@gmail.com>
2019-12-05 18:10:39 +0100

    Auto stash before merge of "master" and "origin/master"

--- Assignment1.m
+++ Assignment1.m
@@ -11,7 +11,8 @@ f = 1.42; % [USD/gallon] 2019 Assuming the same value for the 2014
 LF = 0.75; % Load Factor
 BT = 10*7; % [h/week]
 
-%% Data from excel
+%% Data from excel 
+%The data is extracted from Excel and it is written into variables. 
 
 % Population: It has to be multiplied by 1000, because those values are 
 % given per 1000 inhabitants
@@ -181,7 +182,7 @@ DV = N^2*2+N^2*AC_number+AC_number; % Decision variables
 % There are 2404 decision variables splitted in:
 % - wij that indicate the flow between i and j passing through the hub. (400)
 % - xij that indicate the direct flow between i and j (not going through the hub) (400)
-% - zij^k that indicate the number flight performed by AC^k from i to k. (1600)
+% - zij^k that indicate the number fl ight performed by AC^k from i to k. (1600)
 % - AC^k that indicate the number of aircraft of each type.
 
 obj = zeros(1,DV); % Objective function definition
@@ -234,150 +235,13 @@ ctype = strcat(ctype1);
 
 cplex.addCols(obj, [], lb, ub, ctype); %This can also be done without NameDV
 
-%% Constraint 1: Demand verification
-C1_matrix = zeros(N^2,DV);
-row_c1 = 1;
-for i = 1:N
-    for j = 1:N
-        C1 = zeros(1,DV);
-        C1(varindex_3(1,i,j,k,N,AC_number)) = 1;
-        C1(varindex_3(2,i,j,k,N,AC_number)) = 1;
-        RHS = Demand_2019(i,j);
-        cplex.addRows(-inf,C1,RHS,sprintf('Constraint1%d_%d',i,j));
-        C1_matrix(row_c1,:) = C1; 
-        row_c1 = row_c1 + 1;
-    end
-end
-
-%% Constraint 2: Making sure that w is 0 if the origin or the destination 
-% is the hub
-C2_matrix = zeros(N^2,DV);
-row_c2 = 1;
-for i = 1:N
-    for j = 1:N
-        C2 = zeros(1,DV);
-        C2(varindex_3(1,i,j,k,N,AC_number)) = 1;
-        RHS = Demand_2019(i,j)*g(i)*g(j);
-        cplex.addRows(-inf,C2, RHS,sprintf('Constraint2%d_%d',i,j));
-        C2_matrix(row_c2,:) = C2; 
-        row_c2 = row_c2 + 1;
-    end
-end
-
-%% Constraint 3: Capacity
-
-C3_matrix = zeros(N^2,DV);
-row_c3 = 1;
-for i = 1:N
-    for j = 1:N
-        C3 = zeros(1,DV);
-        C3(varindex_3(2,i,j,k,N,AC_number)) = 1;
-        
-        for m = 1:N
-            C3(varindex_3(1,i,m,k,N,AC_number)) = C3(varindex_3(1,i,m,k,N,AC_number))+(1-g(j));
-            C3(varindex_3(1,m,j,k,N,AC_number)) = C3(varindex_3(1,m,j,k,N,AC_number))+(1-g(i));
-        end
-        
-        
-        for k = 1:AC_number
-            C3(varindex_3(3,i,j,k,N,AC_number)) = -Seats(k)*LF;
-        end
-        C3_matrix(row_c3,:) = C3; 
-        RHS = 0;
-        cplex.addRows(-inf, C3, RHS,sprintf('Constraint3%d_%d',i,j));
-        row_c3 = row_c3 + 1;
-    end
-end
-
-%% Constraint 4: Continuity
-
-C4_matrix = zeros(N*AC_number,DV);
-row_c4 = 1;
-for k = 1:AC_number
-    for i = 1:N
-        C4 = zeros(1,DV);
-        for j = 1:N
-            if i ~= j
-                C4(varindex_3(3,i,j,k,N,AC_number)) = 1;
-                C4(varindex_3(3,j,i,k,N,AC_number)) = -1;
-            end
-        end
-        C4_matrix(row_c4,:) = C4; 
-        cplex.addRows(0,C4,0,sprintf('Constraint4%d_%d',k,i));
-        row_c4 = row_c4 + 1;
-    end
-end
-
-%% Constraint 5: Aircraft productivity
-
-C5_matrix = zeros(AC_number,DV);
-row_c5 = 1;
-for k = 1:AC_number
-    C5 = zeros(1,DV);
-    for i = 1:N
-        for j = 1:N % if i=j the distance is 0 so the first variable should be zero? 
-            if i ~= j
-            C5(varindex_3(3,i,j,k,N,AC_number)) = Distance(i,j)/Speed(k) + TAT(k)*(1+(1-g(j)));
-            end
-            C5(varindex_3(4,i,j,k,N,AC_number)) = -BT;
-        end
-    end
-    C5_matrix(row_c5,:) = C5;
-    cplex.addRows(-inf,C5,0,sprintf('Constraint5%d',k));
-    row_c5 = row_c5 + 1;
-end
-
-%% Constraint 6: Range verification
-C6_matrix = zeros(N^2*AC_number,DV);
-row_c6 = 1;
-for k = 1:AC_number 
-    for i = 1:N
-        for j = 1:N
-            C6 = zeros(1,DV);
-            C6(varindex_3(3,i,j,k,N,AC_number)) = 1;
-            RHS = 0;
-            if Distance(i,j) <= Max_range(k)
-                RHS = 1000000;                
-            end
-            cplex.addRows(-inf, C6, RHS,sprintf('Constraint6%d_%d_%d',k,i,j));
-            C6_matrix(row_c6,:) = C6;
-            row_c6 = row_c6 + 1;
-        end
-    end
-end
-
-%% Constraint 7: Runway verification
-C7_matrix = zeros(N^2*AC_number,DV);
-row_c7 = 1;
-for k = 1:AC_number 
-    for i = 1:N
-        for j = 1:N
-            C7 = zeros(1,DV);
-            C7(varindex_3(3,i,j,k,N,AC_number)) = 1;
-            RHS = 0;
-            if Runway_length(i) >= Rwy_req(k) && Runway_length(j) >= Rwy_req(k)
-                RHS = 1000000;                
-            end
-            cplex.addRows(-inf, C7, RHS,sprintf('Constraint7%d_%d_%d',k,i,j));
-            C7_matrix(row_c7,:) = C7;
-            row_c7 = row_c7 + 1;
-        end
-    end
-end
-
-% %% Constraint 8
-% C8_matrix = zeros(1,DV);
-% row_c8 = 1;
-% C8 = zeros(1,DV);
-% for k = 1:AC_number 
-%     C8(varindex_3(4,i,j,k,N,AC_number)) = 1;
-% end
-% cplex.addRows(-inf, C8, inf,sprintf('Constraint8'));
+%% Constraints
+Constraints(cplex,N,DV,AC_number,Demand_2019,g,Seats,LF,Speed,Distance,TAT,BT,Max_range,Runway_length,Rwy_req)
 
 %% Solve the model
 
 cplex.writeModel([model '.lp']) %Store the model to an .lp file for debugging
-cplex.Param.timelimit.Cur = 300; %Timelimit of the solver in seconds, more useful for larger models
+cplex.Param.timelimit.Cur = 10; %Timelimit of the solver in seconds, more useful for larger models
 cplex.solve();
 
 % for i=1:Nodes
@@ -391,3 +255,89 @@ cplex.solve();
 obj_matrix=cplex.Model.obj;
 A_total=cplex.Model.A;
 RHS_total=cplex.Model.rhs;
+
+%% Post-Processing
+%In this section the KPI performance parameters will be calculated. 
+
+ASK = 0;
+
+Operating_cost_total = 0;
+RPK = 0;
+Revenue_tot = 0;
+for i = 1:N
+    for j = 1:N
+        Lease_cost_tot = 0;
+        for k = 1:AC_number
+            %CASK
+            z_sol = (cplex.Solution.x(varindex_3(3,i,j,k,N,AC_number)));
+            ASK = ASK + z_sol*Distance(i,j)*Seats(k);
+            Lease_cost_tot = Lease_cost_tot + Lease_cost(k)*cplex.Solution.x(varindex_3(4,i,j,k,N,AC_number));
+            
+            if i == j
+                Fixed_costs = 0;
+            else
+                Fixed_costs = CX(k);
+            end
+            Time_costs = CT(k)*Distance(i,j)/Speed(k);
+            Fuel_costs = CF(k)*f/1.5*Distance(i,j);
+            Operating_cost = (Fixed_costs+Time_costs+Fuel_costs)*cplex.Solution.x(varindex_3(3,i,j,k,N,AC_number));
+            
+            if i == pos_hub || j == pos_hub
+                Operating_cost = 0.7*Operating_cost;
+            end
+
+            Operating_cost_total = Operating_cost_total + Operating_cost;
+            
+          
+        end
+        %Revenue Calculation
+        if i == j 
+            Yield = 0;
+        else
+            Yield = 5.9*Distance(i,j)^(-0.76)+0.043;
+        end
+        Revenue_tot = Revenue_tot + Yield*Distance(i,j)*(cplex.Solution.x(varindex_3(1,i,j,k,N,AC_number))+cplex.Solution.x(varindex_3(2,i,j,k,N,AC_number)));
+        
+    end
+end
+
+%RPK
+RPK_x = 0;
+RPK_w = 0;
+for i = 1:N
+    for j = 1:N
+        RPK_x = RPK_x + Distance(i,j)*cplex.Solution.x(varindex_3(2,i,j,k,N,AC_number));
+        RPK_w = RPK_w + (Distance(i,pos_hub)+Distance(pos_hub,j))*cplex.Solution.x(varindex_3(1,i,j,k,N,AC_number));
+    end
+end
+
+Cost_tot = Lease_cost_tot + Operating_cost_total;
+CASK = Cost_tot/ASK;
+RASK = Revenue_tot/ASK;
+RPK = RPK_x + RPK_w;
+LoadFactor = RPK/ASK;
+Yield_tot = (Revenue_tot/RPK);
+BELF = CASK/Yield_tot;
+
+%% MAP
+
+worldmap([41 47], [-89 -84]); 
+lakes=shaperead('worldlakes','UseGeoCoords',true); 
+X_47=(41.709091:0.5:43.127372);
+Y_47=(-87.476139:0.5:-86.340273); 
+griddata(42.419908, -86.91495,1); 
+load coastlines plotm(coastlat,coastlon); 
+plotm(coastlat,coastlon);
+hold on; plot(X_47,Y_47,'r');
+
+worldmap([41 47], [-89 -84]) 
+lakes=shaperead('worldlakes','UseGeoCoords',true); 
+X_47=(41.709091:0.5:43.127372);
+Y_47=(-87.476139:0.5:-86.340273); 
+% griddata(42.419908, -86.91495,1); 
+load coastlines 
+geoshow(coastlat,coastlon); 
+hold on; 
+h = geoshow(X_47',Y_47');
+h.Marker = 'o'; h.Color = 'r';
+
---
+++ Constraints.m
@@ -0,0 +1,144 @@
+function[] = Constraints(cplex,N,DV,AC_number,Demand_2019,g,Seats,LF,Speed,Distance,TAT,BT,Max_range,Runway_length,Rwy_req)
+%% Constraint 1: Demand verification
+%C1_matrix = zeros(N^2,DV);
+%row_c1 = 1;
+k = 1;
+for i = 1:N
+    for j = 1:N
+        C1 = zeros(1,DV);
+        C1(varindex_3(1,i,j,k,N,AC_number)) = 1;
+        C1(varindex_3(2,i,j,k,N,AC_number)) = 1;
+        RHS = Demand_2019(i,j);
+        cplex.addRows(-inf,C1,RHS,sprintf('Constraint1%d_%d',i,j));
+        %C1_matrix(row_c1,:) = C1; 
+%        row_c1 = row_c1 + 1;
+    end
+end
+
+%% Constraint 2: Making sure that w is 0 if the origin or the destination 
+% is the hub
+%C2_matrix = zeros(N^2,DV);
+%row_c2 = 1;
+for i = 1:N
+    for j = 1:N
+        C2 = zeros(1,DV);
+        C2(varindex_3(1,i,j,k,N,AC_number)) = 1;
+        RHS = Demand_2019(i,j)*g(i)*g(j);
+        cplex.addRows(-inf,C2, RHS,sprintf('Constraint2%d_%d',i,j));
+        %C2_matrix(row_c2,:) = C2; 
+        %row_c2 = row_c2 + 1;
+    end
+end
+
+%% Constraint 3: Capacity
+
+%C3_matrix = zeros(N^2,DV);
+%row_c3 = 1;
+for i = 1:N
+    for j = 1:N
+        C3 = zeros(1,DV);
+        C3(varindex_3(2,i,j,k,N,AC_number)) = 1;
+        
+        for m = 1:N
+            C3(varindex_3(1,i,m,k,N,AC_number)) = C3(varindex_3(1,i,m,k,N,AC_number))+(1-g(j));
+            C3(varindex_3(1,m,j,k,N,AC_number)) = C3(varindex_3(1,m,j,k,N,AC_number))+(1-g(i));
+        end
+        
+        
+        for k = 1:AC_number
+            C3(varindex_3(3,i,j,k,N,AC_number)) = -Seats(k)*LF;
+        end
+        %C3_matrix(row_c3,:) = C3; 
+        RHS = 0;
+        cplex.addRows(-inf, C3, RHS,sprintf('Constraint3%d_%d',i,j));
+        %row_c3 = row_c3 + 1;
+    end
+end
+
+%% Constraint 4: Continuity
+
+%C4_matrix = zeros(N*AC_number,DV);
+%row_c4 = 1;
+for k = 1:AC_number
+    for i = 1:N
+        C4 = zeros(1,DV);
+        for j = 1:N
+            if i ~= j
+                C4(varindex_3(3,i,j,k,N,AC_number)) = 1;
+                C4(varindex_3(3,j,i,k,N,AC_number)) = -1;
+            end
+        end
+        %C4_matrix(row_c4,:) = C4; 
+        cplex.addRows(0,C4,0,sprintf('Constraint4%d_%d',k,i));
+        %row_c4 = row_c4 + 1;
+    end
+end
+
+%% Constraint 5: Aircraft productivity
+
+%C5_matrix = zeros(AC_number,DV);
+%row_c5 = 1;
+for k = 1:AC_number
+    C5 = zeros(1,DV);
+    for i = 1:N
+        for j = 1:N % if i=j the distance is 0 so the first variable should be zero? 
+            if i ~= j
+            C5(varindex_3(3,i,j,k,N,AC_number)) = Distance(i,j)/Speed(k) + TAT(k)*(1+(1-g(j)));
+            end
+            C5(varindex_3(4,i,j,k,N,AC_number)) = -BT;
+        end
+    end
+    %C5_matrix(row_c5,:) = C5;
+    cplex.addRows(-inf,C5,0,sprintf('Constraint5%d',k));
+    %row_c5 = row_c5 + 1;
+end
+
+%% Constraint 6: Range verification
+%C6_matrix = zeros(N^2*AC_number,DV);
+%row_c6 = 1;
+for k = 1:AC_number 
+    for i = 1:N
+        for j = 1:N
+            C6 = zeros(1,DV);
+            C6(varindex_3(3,i,j,k,N,AC_number)) = 1;
+            RHS = 0;
+            if Distance(i,j) <= Max_range(k)
+                RHS = 1000000;                
+            end
+            cplex.addRows(-inf, C6, RHS,sprintf('Constraint6%d_%d_%d',k,i,j));
+            %C6_matrix(row_c6,:) = C6;
+            %row_c6 = row_c6 + 1;
+        end
+    end
+end
+
+%% Constraint 7: Runway verification
+%C7_matrix = zeros(N^2*AC_number,DV);
+%row_c7 = 1;
+for k = 1:AC_number 
+    for i = 1:N
+        for j = 1:N
+            C7 = zeros(1,DV);
+            C7(varindex_3(3,i,j,k,N,AC_number)) = 1;
+            RHS = 0;
+            if Runway_length(i) >= Rwy_req(k) && Runway_length(j) >= Rwy_req(k)
+                RHS = 1000000;                
+            end
+            cplex.addRows(-inf, C7, RHS,sprintf('Constraint7%d_%d_%d',k,i,j));
+            %C7_matrix(row_c7,:) = C7;
+            %row_c7 = row_c7 + 1;
+        end
+    end
+end
+
+% %% Constraint 8
+% C8_matrix = zeros(1,DV);
+% row_c8 = 1;
+% C8 = zeros(1,DV);
+% for k = 1:AC_number 
+%     C8(varindex_3(4,i,j,k,N,AC_number)) = 1;
+% end
+% cplex.addRows(-inf, C8, inf,sprintf('Constraint8'));
+
+
+end
