cfd3101183dbc7429f395bff6e9c9b06c98e4ba9
Guillermo <guillermolarajuan@gmail.com>
2020-01-04 17:03:06 +0100

    Some comments and start dynamic programming

--- Assignment 2/Assignment2.m
+++ Assignment 2/Assignment2.m
@@ -57,24 +57,49 @@ for i = 1:N
     end
 end
 
-% To compute the Demand per hour, I think that the best idea would be to
-% compute it when required, not the whole matrix because it would be huge
-% (20x20x24 = 9600)
-
 % Demand - The matrix obtained "Demand_h" gives the demand in each flight
 % depending on the hour. The final matrix has (20x20x240)
-Demand_h = zeros(N,N,h_div);
+% Demand_h = zeros(N,N,h_div);
 indx = 1;
+% for i = 1:N
+%     for j = 1:N
+%         indx = 1;
+%         for h = 1:h_div/10
+%             for p = 1:10
+%                 Demand_h(i,j,indx) = Demand_2014(i,j)*Hour_coeff(i,h);
+%                 indx = indx + 1;
+%             end
+%         end
+%     end
+% end
+
 for i = 1:N
-    for j = 1:N
-        indx = 1;
-        for h = 1:h_div/10
-            for p = 1:10
-                Demand_h(i,j,indx) = Demand_2014(i,j)*Hour_coeff(i,h);
-                indx = indx + 1;
-            end
-        end
+    for j = 1:length(Hour_coeff)
+        Demand_h(i,:,j) = Demand_2014(i,:)*Hour_coeff(i,j);
     end
 end
 
 
+%% Dynamic programming
+time_stages = linspace(24-1/10,0,240);
+% We have to compute the following for each aircraft, maybe we can put a "while" to do it until no aircraft in our fleet is left.
+for aircraft_type = 1:length(Fleet) % To perform the dynamic programming for each aircraft type.
+    possible_routes = 1; % This variable will be used to count the number of possible routes.
+    for t = 1:240/10
+        t_stage = time_stages(t);
+        while t_stage ~= 0 % As we don't know when the aircraft route should finish, we will study all the possible routes finishing in each of the different time stages.
+        % The assumption of the hub-and-spoke airline makes that the first
+        % studied airport, in this case we are starting at the end of the
+        % day so it should be the last one, has to be the hub.
+        
+        
+        % Hacer un loop que siga haciendo lo de dynamic programming para
+        % todos los tiempos y en cada tiempo para todos los aeropuertos
+        
+        % Hacer una matriz que vaya guardando el coste, la secuencia de los
+        % aeropuertos, la secuencia del tiempo, la demanda.
+        
+        % Tal vez sea mejor hacer 
+        end
+    end
+end
